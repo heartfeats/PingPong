16f1876f0e8417b915ab013e22416a1339149491
jm <heartfeats@gmail.com>
2017-09-22 12:58:18 -0700

    add js to clear output and to append to div, stying in css, an image, and content to README

---
+++ README.md
@@ -0,0 +1,26 @@
+# _{Ping Pong}_
+
+#### By _**{John Murray}**_
+
+## Description
+
+_{Counts up to inputted number replacing numbers divisible by 3 or 5 with "ping" and "pong" respectively. Numbers divisible by 15 are replace by "ping pong"}_
+
+## Specifications
+* _Program accepts numbers greater than zero_
+* _Program can count up to the number provided_
+* _Program can replace numbers divisible by 3 with "Ping"_
+* _Program can replace numbers divisible by 5 with "Pong"_
+* _Program can replace numbers divisible by 15 with "Ping Pong"_
+* _Program can clear output at users discretion_
+
+
+## Setup/Installation Requirements
+* _No setup required_
+* _Open in Browser_
+
+### License
+
+*{MIT License}*
+
+Copyright (c) 2017 **_{John Murray}_**
--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,27 @@
 $(function() {
+  $("#clear").click(function() {
+    $("#output").html('');
+  });
   $('#formOne').submit(function(event) {
     var userInput = $('input#inputOne').val()
 
     for (var i = 1; i <= userInput; i++) {
       if (i % 15 === 0) {
-        document.write('Ping Pong');
+        // document.write('Ping Pong');
+        $("#output").append('Ping Pong');
+
       }
       else if (i % 3 === 0) {
-        document.write('Ping');
+        $("#output").append('Ping');
       }
       else if (i % 5 === 0) {
-        document.write('Pong')
+        $("#output").append('Pong')
       }
       else {
-        document.write(i)
+        $("#output").append(i)
       }
-      document.write('<br>')
+      $("#output").append('<br>')
     }
-    
     event.preventDefault();
   });
 });
